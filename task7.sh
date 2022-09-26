252dffe4ee48459bbb5fcc0d64bb93c76c96fb68
Cole Nussear <cnussear1@gulls.salisbury.edu>
2022-09-26 13:43:29 -0400

    general updates

--- task7.sh
+++ task7.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+#asks the user for a number
 echo "please enter a number"
 read num
 
+#initializing a factorial variable that keeps track of the profit
 factorial=1
 
+#loop that goes through the factorial
 while [ $num -gt 1 ];
 do 
+	#factorial is calcualted by multiplying the factorial number by the current number
 	factorial=`expr $factorial \* $num`
+	#number is decremented by 1 to simulate factorial 
 	num=`expr $num - 1`
 done
+#prints the factorial number
 echo $factorial 
 
--- task8.sh
+++ task8.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 
-
+#if there are no parameters passed, then the program will give the user a message and exit
 if [ $# -eq 0 ] 
 then 
 	echo "no argument detected"
 	exit 0; 
 fi 
+#if there is more than one passed, then the program will give the user a message and exit
 if [ $# -gt 1 ]
 then 
 	echo "more than one argument"
 	exit 0; 
 fi 
+#variable num is the value of the parameter passed
 num=$1
 
+#sum variable to keep track of the sum of the number
 sum=0
+#loop to get the sum
 while [ $num -gt 0 ];
 do
+       #the variable temp is intialized by finding the modulus of the number by 10
        temp=`expr $num % 10`
+       #temp is subtracted from num in order to get the next number
+       num=`expr $num - $temp`
+       #num is then divided by 10 to get the next number posistion 
        num=`expr $num / 10`
+       #temp is added to the current sum 
        sum=`expr $sum + $temp`
 done
+#printing of the sum of all numbers
 echo $sum
 exit 0;
 	
